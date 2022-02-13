1f2bd4d484151e3acef2f2128ae4d7116e1f7a4c
SigmaStar <sigmastar00@gmail.com>
2022-02-13 13:24:13 -0800

    Add bonk

--- Player.gd
+++ Player.gd
@@ -15,6 +15,8 @@ export var friction: float
 export var gravity: float
 export var spin_modifier: float
 
+export var ouch_length: int
+export var ouch_speed: float
 
 # == PUBLIC VARIABLES ==
 
@@ -24,6 +26,10 @@ var _velocity := Vector2.ZERO
 var _dash_frames := 0
 var _dashing := false
 var _dash_vector: Vector2
+
+var _ouch_mode := false
+var _ouch_vector: Vector2
+var _ouch_frames := 0
 
 # == ONREADY VARIABLES ==
 onready var _line := $Line2D as Line2D
@@ -41,22 +47,36 @@ func _ready()  -> void:
 	
 	
 func _physics_process(delta: float) -> void:
+	if _ouch_frames >= ouch_length:
+		_ouch_mode = false
+		_ouch_frames = 0
+	
 	if _dash_frames >= dash_length:
 		_dashing = false
 		_dash_frames = 0
 	
-	if not _dashing and Input.is_action_just_pressed("dash"):
+	if not _ouch_mode and not _dashing and Input.is_action_just_pressed("dash"):
 		_dash_vector = _get_move_vector()
 		_dashing = true
 	
 	if _dashing:
 		_velocity = dash_speed * _dash_vector
 		_dash_frames += 1
+	elif _ouch_mode:
+		_velocity = ouch_speed * _ouch_vector
+		_ouch_frames += 1
 	else:
 		_velocity.y += gravity * delta
 		_velocity.x = move_toward(_velocity.x, 0, friction * delta)
 		
 	_velocity = move_and_slide(_velocity)
+	
+	if not _ouch_mode and get_slide_count() > 0:
+		var collision := get_slide_collision(0)
+		_dashing = false
+		_ouch_mode = true
+		_ouch_vector = collision.normal
+		
 	
 
 func _process(delta: float) -> void:
@@ -65,7 +85,12 @@ func _process(delta: float) -> void:
 	var bonus_spin = 4 if _dashing else 1
 	_sprite.rotate(_velocity.length() * bonus_spin * spin_modifier * delta)
 	_sparkles.emitting = _dashing
-	_sprite.modulate = Color(1.5, 1.5, 1.6) if _dashing else Color.white
+	if _dashing:
+		_sprite.modulate = Color(1.5, 1.5, 1.6)
+	elif _ouch_mode:
+		_sprite.modulate = Color(1, 0.1, 0.1) 
+	else:
+		_sprite.modulate = Color.white
 
 
 # == PUBLIC METHODS ==
